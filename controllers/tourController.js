570b42e70fe116f94d97c4c537c8d91bb4f7d6cd
sung8 <heechuw@gmail.com>
2020-09-05 02:57:39 -0400

    added deleting tour with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,47 +1,6 @@
 // const fs = require('fs');
 const Tour = require('./../models/tourModels');
 
-// commented out bc just for reference
-// top-level code (only executed once)
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// check id
-// removes the repeated code of ID check in getTour, updateTour, and deleteTour
-// this middleware is not part of our pipeline/
-// might that we can simply create a function to check ID and call it inside each of the route handlers, but that goes against the philosophy of Express
-// .... so we should always work with the middleware stack/pipeline as much as we can
-// each of the route handlers don't have to worry about checking id... it just does its job
-// also ID would be automatically checked if we add another controller that depends on id param
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     // if id is invalid, make sure to RETURN so the req-res cycle ends here and doesn't hit next()
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// checkBody middleware
-// check if the body contains the name and price property
-// if not, send back 400 (bad request) response
-// add it to post handler stack
-// exports.checkBody = (req, res, next) => {
-//   //console.log(`Tour id is: ${val}`);
-//   if (!req.body.name || !req.body.price) {
-//     // if id is invalid, make sure to RETURN so the req-res cycle ends here and doesn't hit next()
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//   }
-//   next();
-// };
-
 // ROUTE HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
@@ -170,9 +129,20 @@ exports.updateTour = async (req, res) => {
 
 // delete tour
 // (not actually implemented bc we don't want to delete our json data)
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    // no need to store it in a variable
+    // in RESTful api, it is common practice to not send back any data to the client when there's a delete operation
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err,
+    });
+  }
 };
